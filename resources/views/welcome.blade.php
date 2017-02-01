b8a050f96bb7c0c236dcc3b2c801f1a212ae166f
vagrant <bazookia@gmail.com>
2017-02-01 21:44:54 +0000

    added phpstorm stuff... and also added the 'auth' scaffold.  And changed font to Open Sans

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,14 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
         <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
                 color: #636b6f;
-                font-family: 'Raleway', sans-serif;
+                font-family: 'Open Sans', sans-serif;
                 font-weight: 100;
                 height: 100vh;
                 margin: 0;
