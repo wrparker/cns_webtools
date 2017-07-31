30faebb77ee541aba81b9ccbbf8ffe70630bb45e
W. Ryan Parker <wrparker@utexas.edu>
2017-07-31 10:55:19 -0500

    Fixed initialization script to add appropriate permissioning.

--- fix-permissions.bash
+++ fix-permissions.bash
@@ -1,4 +1,3 @@
 find . -type f -exec chmod 664 {} \;    
 find . -type d -exec chmod 775 {} \;
-chgrp -R www-data storage bootstrap/cache;
 chmod -R ug+rwx storage bootstrap/cache;
--- init.bash
+++ init.bash
@@ -2,4 +2,5 @@ mkdir bootstrap/cache/;
 composer install;
 #cp .env.example .env;
 php artisan key:generate;
+bash fix-permissions.bash
 
